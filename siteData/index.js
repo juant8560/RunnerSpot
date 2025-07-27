34ad03ff920fcc3bcc70f06e49457e101b83ec27
Ceylinsantos <cesantos020@unicah.edu>
2025-07-27 00:53:32 -0600

    Arreglos de conflictos

--- siteData/index.js
+++ siteData/index.js
@@ -4,13 +4,9 @@ import faqData from "./pages/faq.js";
 import testimonialsData from "./pages/testimonials.js";
 import galeriatestimonioData from './pages/galeriatestimonio.js';
 import formtestimoniosData from './pages/formtestimonios.js';
-<<<<<<< HEAD
 import eventsData from './pages/events.js';
 import challengesData from './pages/challenges.js';
-
-=======
 import consumiblesData from "./pages/consumibles.js";   
->>>>>>> 109f0b00cbc1402241269d3c5118821982e1468f
 function context(page) {
     console.log("Getting context for: " + page);
     let context = {
